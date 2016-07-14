19dd7644a045ca4913b13ce51c3d19a40ecd416d
Mwithi <alessandro.domanico@yahoo.it>
2016-07-14 12:08:01 +0300

    One2Many limit enabler: - fixed module description

--- o2m_limit_enabler/__openerp__.py
+++ o2m_limit_enabler/__openerp__.py
@@ -3,20 +3,21 @@
     'category': 'Extra Tools',
     'version': '0.1',
     'license': 'AGPL-3',
-    'description':
-        """
-One2Many limit enabler.
-=======================
+    'description': """
+
+One2Many limit enabler
+======================
 
 This module enables 'limit' attribute on one2many fields in order to set the pagination limit.
 
 Example:
-
+--------
 <field name="MyOne2ManyField" limit="123">
     <tree string="MyField">
         ...
     </tree>
-        """,
+
+""",
     'author': 'Alessandro Domanico (alessandro.domanico@informaticisenzafrontiere.org)',
     'website': 'www.informaticisenzafrontiere.org',
     'license': 'AGPL-3',
@@ -25,7 +26,7 @@ Example:
         'static/src/js/view_form.js',
     ],
     'images': [
-        'description/o2m_screenshot.jpg',
+        'static/description/o2m_screenshot.jpg',
     ],
     'installable' : True,
 }
